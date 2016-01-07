5d62e63890e56ad389db3dc7fc3cea2ab4ecb064
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-01-07 23:10:53 +0100

    Add fastd peers and signing keys to site.conf

--- genonce.py
+++ genonce.py
@@ -4,17 +4,17 @@ import yaml
 import hashlib
    
 def getMacFromString(string):
+    stringHash = hashlib.sha1(string.encode()).hexdigest()
     return \
-	string[:2] + ':' + string[2:4] + ':' + string[4:6] \
-	+ ':' + string[8:10] + ':' + string[12:14] + ':' + string[16:18]
+	stringHash[:2] + ':' + stringHash[2:4] + ':' + stringHash[4:6] \
+	+ ':' + stringHash[8:10] + ':' + stringHash[12:14] + ':' + stringHash[16:18]
 
 sitesInHandle=open('files/sites.yml.in', 'r')
 
 sites=yaml.load(sitesInHandle)
 for site_code in sites['sites'].keys():
-    site_hash = hashlib.sha1(site_code.encode()).hexdigest()
-    sites['sites'][site_code]['bssid'] = getMacFromString('bssid' + site_hash)
-    sites['sites'][site_code]['next_mac'] = getMacFromString('next_mac' + site_hash)
+    sites['sites'][site_code]['bssid'] = getMacFromString('bssid' + site_code)
+    sites['sites'][site_code]['next_mac'] = getMacFromString('next_mac' + site_code)
 
 #print (yaml.dump(sites, default_flow_style=False))
 sitesOutHandle=open('files/sites.yml', 'w')
