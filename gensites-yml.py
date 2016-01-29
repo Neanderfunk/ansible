826928d05d8cbf5a038cfd2eb1d840f640cb35bc
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-01-29 23:55:44 +0100

    fix dhcp, fastd, bridge configuration

--- gensites-yml.py
+++ gensites-yml.py
@@ -13,18 +13,20 @@ def getMacFromString(string):
 sitesInHandle=open('vars/sites.yml.in', 'r')
 
 sites=yaml.load(sitesInHandle)
+
+for node in range(len(sites['supernodes'])):
+  sites['supernodes'][node]['fqdn'] = sites['supernodes'][node]['host'] + sites['supernodes'][node]['domain']
+
 for site_code in sites['sites'].keys():
     sites['sites'][site_code]['bssid'] = getMacFromString('bssid' + site_code)
     sites['sites'][site_code]['next_mac'] = getMacFromString('next_mac' + site_code)
     sites['sites'][site_code]['site_name'] = 'Freifunk ' + sites['sites'][site_code]['site']
+
     ip = {
         '4': ipaddr.IPNetwork(sites['sites'][site_code]['compressed4']),
         '6': ipaddr.IPNetwork(sites['sites'][site_code]['compressed6'])
     }
 
-    nrSupernodes=len(sites['sites'][site_code]['fastd'])
-    # more is senseless, but causes problem with next hop (v4)
-    assert(nrSupernodes < 255);
     supernodes={}
     
     assert(ip['6'].prefixlen <= 64);
@@ -36,14 +38,34 @@ for site_code in sites['sites'].keys():
         sites['sites'][site_code]['netmask' + ver] = str(ip[ver].netmask)
 
         hostaddr=ip[ver].iterhosts()
-        supernodes[ver]=list((sn, next(hostaddr)) for sn in sites['supernodes'])
+        supernodes[ver]=list((sn['host'], next(hostaddr)) for sn in sites['supernodes'])
 
         sites['sites'][site_code]['supernodeAddr' + ver] = \
 			dict((node[0], str(node[1])) for node in supernodes[ver])
     
     sites['sites'][site_code]['next_node4'] = str(supernodes['4'][0][1] + 254)
     sites['sites'][site_code]['next_node6'] = str(supernodes['6'][0][1] + 0xfffe)
+
+    sites['sites'][site_code]['dhcp'] = {'range': {}}
+    currentIP=supernodes['4'][0][1] + sites['sites'][site_code]['reserved_ip_addresses']
+    for sn in sites['supernodes']:
+      sites['sites'][site_code]['dhcp']['range'][sn['host']] = {'from': str(currentIP) }
+      currentIP += int((ip['4'].numhosts - sites['sites'][site_code]['reserved_ip_addresses'] - 2) / len(sites['supernodes']))
+      sites['sites'][site_code]['dhcp']['range'][sn['host']]['to'] = str(currentIP-1)
+
     sites['sites'][site_code]['bridge'] = 'br0' if site_code == 'ffnef-met' else 'br-' + site_code.split('-')[-1]
+
+    sites['sites'][site_code]['fastd'] = {
+			'peers': sites['sites'][site_code]['fastd'],
+			'limit': 200,
+			'mtu': 1364,
+			'interface': 'bat-' + site_code.split('-')[-1],
+			'mac': dict((node[0], getMacFromString('fastd'+site_code+node[0])) for node in supernodes[ver])
+			}
+    sites['sites'][site_code]['batman'] = {
+			'mac': dict((node[0], getMacFromString('batman'+site_code+node[0])) for node in supernodes[ver]),
+    		'interface': 'tap-' + site_code.split('-')[-1]
+			}
 
         
 
