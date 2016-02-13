65b31584bc2f862ffa2400403ea568d3c71d4497
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-02-13 14:41:32 +0100

    connect to ffdus, correct update IP, add key to images

--- gensites-yml.py
+++ gensites-yml.py
@@ -16,13 +16,25 @@ sitesInHandle=open('vars/sites.yml.in', 'r')
 
 sites=yaml.load(sitesInHandle)
 
+sites['is_supernode'] = {}
+sites['own_sites'] = {}
+
 for node in range(len(sites['supernodes'])):
   sites['supernodes'][node]['fqdn'] = sites['supernodes'][node]['host'] + sites['supernodes'][node]['domain']
+  sites['is_supernode'][sites['supernodes'][node]['host']] = True
 
 for site_code in sites['sites'].keys():
+    foreign = True
+    if site_code.find('-') >= 0:
+        if site_code.split('-')[0] == sites['config']['my_meta_community']:
+            foreign = False
+    
+    sites['sites'][site_code]['foreign'] = foreign
+
     sites['sites'][site_code]['bssid'] = getMacFromString('bssid' + site_code)
-    sites['sites'][site_code]['next_mac'] = getMacFromString('next_mac' + site_code)
-    sites['sites'][site_code]['site_name'] = 'Freifunk ' + sites['sites'][site_code]['site']
+    if not foreign:
+	sites['sites'][site_code]['next_mac'] = getMacFromString('next_mac' + site_code)
+	sites['sites'][site_code]['site_name'] = 'Freifunk ' + sites['sites'][site_code]['site']
 
     ip = {
         '4': ipaddr.IPNetwork(sites['sites'][site_code]['compressed4']),
@@ -40,32 +52,37 @@ for site_code in sites['sites'].keys():
         sites['sites'][site_code]['netmask' + ver] = str(ip[ver].netmask)
 
         hostaddr=ip[ver].iterhosts()
-        supernodes[ver]=list((sn['host'], next(hostaddr)) for sn in sites['supernodes'])
+	if not foreign:
+	    supernodes[ver]=list((sn['host'], next(hostaddr)) for sn in sites['supernodes'])
 
-        sites['sites'][site_code]['supernodeAddr' + ver] = \
-            dict((node[0], str(node[1])) for node in supernodes[ver])
-    
-    sites['sites'][site_code]['next_node4'] = str(supernodes['4'][0][1] + 254)
-    sites['sites'][site_code]['next_node6'] = str(supernodes['6'][0][1] + 0xfffe)
+	    sites['sites'][site_code]['supernodeAddr' + ver] = \
+		dict((node[0], str(node[1])) for node in supernodes[ver])
+   
+    if not foreign:
+	sites['sites'][site_code]['next_node4'] = str(supernodes['4'][0][1] + 254)
+	sites['sites'][site_code]['next_node6'] = str(supernodes['6'][0][1] + 0xfffe)
 
-    sites['sites'][site_code]['dhcp'] = {'range': {}}
-    currentIP=supernodes['4'][0][1] + sites['sites'][site_code]['reserved_ip_addresses']
+	sites['sites'][site_code]['dhcp'] = {'range': {}}
+	currentIP=supernodes['4'][0][1] + sites['sites'][site_code]['reserved_ip_addresses']
     for sn in sites['supernodes']:
-      sites['sites'][site_code]['dhcp']['range'][sn['host']] = {'from': str(currentIP) }
-      currentIP += int((ip['4'].numhosts - sites['sites'][site_code]['reserved_ip_addresses'] - 2) / len(sites['supernodes']))
-      sites['sites'][site_code]['dhcp']['range'][sn['host']]['to'] = str(currentIP-1)
+	if not foreign:
+	  sites['sites'][site_code]['dhcp']['range'][sn['host']] = {'from': str(currentIP) }
+	  currentIP += int((ip['4'].numhosts - sites['sites'][site_code]['reserved_ip_addresses'] - 2) / len(sites['supernodes']))
+	  sites['sites'][site_code]['dhcp']['range'][sn['host']]['to'] = str(currentIP-1)
 
     sites['sites'][site_code]['bridge'] = 'br0' if site_code == 'ffnef-met' else 'br-' + site_code.split('-')[-1]
 
     sites['sites'][site_code]['fastd'] = {
-            'peers': sites['sites'][site_code]['fastd'],
-            'limit': 200,
-            'mtu': 1364,
-            'interface': 'bat-' + site_code.split('-')[-1],
-            'mac': dict((node[0], getMacFromString('fastd'+site_code+node[0])) for node in supernodes[ver])
-            }
+		'peers': sites['sites'][site_code]['fastd'],
+		'limit': 200,
+		'mtu': 1364,
+		'interface': 'bat-' + site_code.split('-')[-1],
+		'mac': (False if foreign else dict((node[0],
+		    getMacFromString('fastd'+site_code+node[0])) for node in supernodes[ver]))
+		}
     sites['sites'][site_code]['batman'] = {
-            'mac': dict((node[0], getMacFromString('batman'+site_code+node[0])) for node in supernodes[ver]),
+            'mac': (False if foreign else dict((node[0],
+		getMacFromString('batman'+site_code+node[0])) for node in supernodes[ver])),
             'interface': 'tap-' + site_code.split('-')[-1]
             }
    
@@ -76,22 +93,26 @@ remote ipv4 "{{ host }}" port {{ port }};
     i=0
     try:
       os.mkdir ('out/fastd-peers/')
-    except FileExistsError:
+    except Exception:
       True
 
-    for peer in sites['sites'][site_code]['fastd']['peers']:
-        try:
-            os.mkdir ('out/fastd-peers/' + site_code[2:])
-        except FileExistsError:
-          True
+    if not foreign:
+	sites['own_sites'][site_code] = sites['sites'][site_code]
 
-        peerFile = open('out/fastd-peers/' + site_code[2:] + '/' +
-            site_code.split('-')[1] + str(i), 'w')
-        peerFile.write (fastd_template.render(host=peer,
-            key=sites['sites'][site_code]['fastd']['peers'][peer]['key'],
-            port=sites['sites'][site_code]['fastd']['peers'][peer]['port']))
-        peerFile.close()
-        i = i+1
+	for peer in sites['sites'][site_code]['fastd']['peers']:
+	    try:
+		os.mkdir ('out/fastd-peers/' + site_code[2:])
+	    except Exception:
+	      True
+
+	    if site_code.find('-') >= 0:
+		peerFile = open('out/fastd-peers/' + site_code[2:] + '/' +
+		    site_code.split('-')[1] + str(i), 'w')
+		peerFile.write (fastd_template.render(host=peer,
+		    key=sites['sites'][site_code]['fastd']['peers'][peer]['key'],
+		    port=sites['sites'][site_code]['fastd']['peers'][peer]['port']))
+		peerFile.close()
+		i = i+1
 
         
 
