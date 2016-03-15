64ac6eaf2fc4d88cdd5ef4d7a9e3fb2b5709a3b2
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-03-16 00:07:29 +0100

    create aliases.json

--- gensites-yml.py
+++ gensites-yml.py
@@ -5,6 +5,7 @@ import hashlib
 import ipaddr
 import jinja2
 import os
+import json
    
 def getMacFromString(string):
     stringHash = hashlib.sha1(string.encode()).hexdigest()
@@ -12,16 +13,34 @@ def getMacFromString(string):
     stringHash[0] + '0:' + stringHash[2:4] + ':' + stringHash[4:6] \
     + ':' + stringHash[8:10] + ':' + stringHash[12:14] + ':' + stringHash[16:18]
 
+def mac2IPv6linklocal(mac):
+    # Remove the most common delimiters; dots, dashes, etc.
+    mac_value = int(mac.replace(':', ''), 16)
+    #
+    # Split out the bytes that slot into the IPv6 address
+    # XOR the most significant byte with 0x02, inverting the 
+    # Universal / Local bit
+    high2 = mac_value >> 32 & 0xffff ^ 0x0200
+    high1 = mac_value >> 24 & 0xff
+    low1 = mac_value >> 16 & 0xff
+    low2 = mac_value & 0xffff
+    #
+    return 'fe80::{:04x}:{:02x}ff:fe{:02x}:{:04x}'.format(high2, high1, low1, low2)
+
 sitesInHandle=open('vars/sites.yml.in', 'r')
 
 sites=yaml.load(sitesInHandle)
 
 sites['is_supernode'] = {}
+sites['supernodesByName'] = {}
 sites['own_sites'] = {}
 
 for node in range(len(sites['supernodes'])):
-  sites['supernodes'][node]['fqdn'] = sites['supernodes'][node]['host'] + '.' + sites['supernodes'][node]['domain']
-  sites['is_supernode'][sites['supernodes'][node]['host']] = True
+  hostname = sites['supernodes'][node]['host']
+  sites['supernodes'][node]['fqdn'] = hostname + '.' + sites['supernodes'][node]['domain']
+  sites['is_supernode'][hostname] = True
+
+  sites['supernodesByName'][hostname] = sites['supernodes'][node]
 
 for site_code in sites['sites'].keys():
     foreign = True
@@ -122,3 +141,71 @@ sites['site_partitions'] = { 'all': sites['sites'], 'own': sites['own_sites'] }
 sitesOutHandle=open('vars/sites.yml', 'w')
 sitesOutHandle.write (yaml.dump(sites, default_flow_style=False))
 sitesOutHandle.close()
+
+for site_code in sites['own_sites']:
+	aliases={}
+	for node in sites['sites'][site_code]['fastd']['mac']:
+		if 'installed' in sites['supernodesByName'][node].keys():
+			mac = sites['sites'][site_code]['fastd']['mac'][node]
+			batmanMac = sites['sites'][site_code]['batman']['mac'][node]
+			compactMac = mac.replace(':', '')
+			aliases[compactMac] = {
+	"firstseen": sites['supernodesByName'][node]['installed'],
+    "nodeinfo": {
+      "owner": {
+        "contact": "admin@neanderfunk.de"
+      },
+      "node_id": compactMac,
+      "network": {
+        "mac": mac,
+        "mesh_interfaces": [
+          mac,
+          batmanMac,
+        ],
+        "addresses": [
+         sites['sites'][site_code]['supernodeAddr4'][node],
+         sites['sites'][site_code]['supernodeAddr6'][node],
+		 mac2IPv6linklocal(mac),
+        ],
+        "mesh": {
+          sites['sites'][site_code]['fastd']['interface']: {
+            "interfaces": {
+              "tunnel": [
+                batmanMac,
+              ],
+              "other": [
+                mac,
+              ]
+            }
+          }
+        }
+      },
+      "hostname": node + ".ffnef.de",
+      "hardware": {
+        "model": "esxi",
+        "nproc": 2
+      },
+      "software": {
+        "batman-adv": {
+          "compat": 15,
+          "version": "2015.1"
+        },
+        "fastd": {
+          "enabled": True
+        },
+        "autoupdater": {
+          "enabled": True,
+        },
+        "firmware": {
+          "base": "Ubuntu",
+          "release": "15.10"
+        }
+      },
+      "system": {
+        "site_code": site_code
+      }
+	}
+	}
+	aliasesOutHandle=open('out/aliases.json/aliases.json.' + site_code, 'w')
+	aliasesOutHandle.write(json.dumps(aliases, indent=2))
+	aliasesOutHandle.close()
